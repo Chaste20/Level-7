0bdf54e9079a9d755735ba8ebe9925bf24e3744f
Chaste20 <chastesauveur@gmail.com>
2023-01-05 18:17:04 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('./app');
-app.listen(process.env.PORT || 3000, ()=>{
-  console.log('started express server');
+const app = require("./app");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server listening at port - ${port}`);
 });
